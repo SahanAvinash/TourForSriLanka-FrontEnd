456eb6be3d4475a0e002d96210653d764abe486a
SahanAvinash <sahanavinash00@gmail.com>
2026-06-25 16:40:44 +0530

    traveler register form fully done.validation and connect with back end all of are done

--- tour-for-sri-lanka/src/App.jsx
+++ tour-for-sri-lanka/src/App.jsx
@@ -5,24 +5,27 @@ import HomePage from './pages/homePage';
 import LoginPage from './pages/loginPage';
 import {Toaster} from "react-hot-toast"
 import RegisterRole from './pages/register/registerRole';
-import TravelerRegister from './pages/register/travelerRegister';
+import TravelerRegister from './pages/register/traveler_register/travelerRegister';
 import VehicleOwnerRegister from './pages/register/vehicleOwnerRegister';
 import GuideRegister from './pages/register/guideRegister';
 import HotelOwnerRegister from './pages/register/hotelOwnerRegister';
+import TravelerProfilePhoto from './pages/register/traveler_register/travelerProfilePhoto';
+
 
 function App() {
   return (
     <BrowserRouter>
     <Toaster/>
       <Routes path="/*">
+        <Route path="/*" element={<HomePage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/register-role" element={<RegisterRole/>}/>
-        <Route path="admin/*" element={<AdminPage/>}/>
-        <Route path="/*" element={<HomePage/>}/>
         <Route path="/traveler-register" element={<TravelerRegister/>}/>
+        <Route path="/travelerprofilephoto" element={<TravelerProfilePhoto/>}/>
         <Route path="/vehicle-register" element={<VehicleOwnerRegister/>}/>
         <Route path="/guide-register" element={<GuideRegister/>}/>
         <Route path="/hotel-register" element={<HotelOwnerRegister/>}/>
+        <Route path="admin/*" element={<AdminPage/>}/>
       </Routes>
     </BrowserRouter>
   )
---
+++ tour-for-sri-lanka/src/pages/register/traveler_register/travelerProfilePhoto.jsx
@@ -0,0 +1,183 @@
+import { FaCamera } from "react-icons/fa";
+import { useState, useRef } from "react";
+import { GrFormNextLink, GrFormPreviousLink } from "react-icons/gr";
+import { useNavigate } from "react-router-dom";
+
+export default function TravelerProfilePhoto(){
+    const [photo,setPhoto] = useState(null)
+    const [photoFile,setPhotoFile] = useState(null)
+    const fileInputRef = useRef(null)
+
+    const [err,setErr] = useState("")
+    
+    const navigate = useNavigate()
+
+    const handleDrop = (e) =>{
+        e.preventDefault()
+
+        const file = e.dataTransfer.files[0]
+
+        if(!file) return
+
+        if(!["image/jpeg", "image/png"].includes(file.type)){
+            setErr("Only JPG and PNG files are allowed")
+            return
+        }
+        if(file.size > 2*1024*1024){
+            setErr("File size must be less than 2MB")
+            return
+        }
+        setPhoto(URL.createObjectURL(file))
+        setPhotoFile(file)
+    }
+    const handleDragOver = (e) => {
+        e.preventDefault()
+    }
+    const handlePhotoChange = (e)=>{
+        const file = e.target.files[0]
+
+        if(!file) return
+
+        if(!["image/jpeg","image/png"].includes(file.type)){
+            setErr("Only JPG and PNG files are allowed")
+            return
+        }
+        if(file.size >2*1024*1024){
+            setErr("File size must be less than 2MB")
+            return
+        }
+        setPhoto(URL.createObjectURL(file))
+        setPhotoFile(file)
+    }
+    const handleRemovePhoto = ()=>{
+        setPhoto(null)
+        setPhotoFile(null)
+
+        if(fileInputRef.current){
+            fileInputRef.current.value=""
+        }
+    }
+
+    const handlePrevious = () =>{
+        navigate(-1)
+    }
+    const handleSignUp = async () =>{
+        try{
+            const saved = JSON.parse(sessionStorage.getItem("TravelerRegister"))
+
+            if(!saved){
+                setErr("Something went wrong")
+                return
+            }
+            let imageBase64 = null
+            if(photoFile){
+                imageBase64 = await new Promise((resolve, reject)=>{
+                    const reader = new FileReader()
+                    reader.onload = () => resolve(reader.result)
+                    reader.onerror = reject
+                    reader.readAsDataURL(photoFile)
+                })
+            }
+            const travelData = {
+                role : saved.role,
+                firstName : saved.firstName,
+                lastName : saved.lastName,
+                email : saved.email,
+                password : saved.password,
+                NIC : saved.NIC,
+                country : String(saved.country),
+                mobile : saved.mobile,
+                image : imageBase64
+            }
+            const response = await fetch("http://localhost:3000/api/traveler/register",{
+                method : "POST",
+                headers: { "Content-Type": "application/json" },
+                body : JSON.stringify(travelData)
+            })
+            const data = await response.json()
+
+            if(!response.ok){
+                setErr(data.message || "Signup failed.  Please try again later")
+                return
+            }
+            localStorage.setItem("token",data.token)
+            sessionStorage.removeItem("TravelerRegister")
+            navigate("/login")
+        }catch(error){
+            setErr("Somthing went wrong. Please try again"+ error.message)
+        }
+    }
+    
+    return(
+
+         <div className="w-full h-screen bg-gradient-to-r from-[#06141B] to-[#253745] flex justify-center items-center relative">
+
+            <div className="absolute left-[80px]">
+
+                <img src="/main_logo.png" alt="main_logo.png"/>
+
+            </div>
+
+            <div className="w-[500px] h-[380px] bg-[#253745] text-[#CCD0CF] absolute right-[10%] rounded-[20px] flex flex-col items-center">
+                
+                <h1 className="text-[25px] mt-[20px] font-bold text-[#CCD0CF]">Sign up as a Traveler</h1>
+                {err && (
+                    <div className="text-[12px] text-[#9E4444]">
+                        {err}
+                    </div>
+                )}
+
+                <div className="w-full flex justify-between items-center p-[30px] mt-[10px]">
+
+                    <div className="w-[110px]">
+
+                        <h2 className="font-bold text-[#CCD0CF] text-[16px]">Profile Photo</h2>
+
+                        <h1 className="text-[12px] text-[#CCD0CF]/80 pt-[10px]">Upload a clear photo of yourself</h1>
+
+                        <h1 className="text-[12px] text-[#CCD0CF]/80 pt-[10px]">JPG,PNG format Max size 2MB</h1>
+
+                    </div>
+
+                    <div onClick={()=> fileInputRef.current.click()}
+                    onDragOver={handleDragOver}
+                    onDrop={handleDrop} 
+                    className="w-[120px] h-[120px] overflow-hidden bg-[#4A5C6A]/50 rounded-full border-2 border-dotted border-[#00C896]/50 flex flex-col justify-center items-center cursor-pointer group hover:border-[#00C896]/80 transition-all duration-300">
+
+                        {photo ? (
+                            <img
+                                src={photo}
+                                alt="profile"
+                                className="w-full h-full object-cover"
+                            />
+                        ) : (
+                            <>
+                                <FaCamera className="text-[#00C896]/50 text-[20px] group-hover:text-[#00C896]/80 transition-all duration-300"/>
+
+                                <p className="text-[10px] text-[#CCD0CF]/50 mt-[5px] text-center pl-[5px] pr-[5px]">Drag & Drop your photo</p>
+                                <p className="text-[10px] text-[#CCD0CF]/50 text-center">or</p>
+                                <p className="text-[#00C896]/50 text-[10px] group-hover:text-[#00C896]/80 transition-all duration-300">Browse Files</p>
+                            </>
+                        )}
+                    </div>
+                    <input
+                        type="file"
+                        accept="image/png,image/jpeg"
+                        ref={fileInputRef}
+                        className="hidden"
+                        onChange={handlePhotoChange}
+                    />
+                    <div className="flex flex-col">
+                        <button onClick={() => fileInputRef.current.click()} className="rounded-[20px] bg-[#4A5C6A]/50 p-[5px] text-[12px] text-[#CCD0CF] border border-[#4A5C6A]/50 hover:border-[#00C896]/80 transition-all duration-300">Change Photo</button>
+                        <button onClick={handleRemovePhoto} className="rounded-[20px] bg-[#4A5C6A]/50 p-[5px] text-[12px] text-[#CCD0CF] mt-[10px] border border-[#4A5C6A]/50 hover:border-[#9E4444]/80 transition-all duration-300">Remove Photo</button>
+                    </div>
+                </div>
+                        <button onClick={handleSignUp} className="text-[20px] font-bold w-[90%] h-[50px] flex justify-center items-center rounded-[20px] bg-[#00C896]/50 hover:bg-[#00C896]/80 transition-all duration-300 hover:scale-105">Sign Up</button>
+                        <button onClick={handlePrevious} className="w-[90%] h-[50px] flex justify-center items-center rounded-[20px] bg-[#4A5C6A]/50 hover:bg-[#4A5C6A]/80 transition-all duration-300 mt-[10px] hover:scale-95 text-[20px] font-bold"><GrFormPreviousLink/>Previous</button>
+            </div>
+
+         </div>
+
+    )
+
+}
